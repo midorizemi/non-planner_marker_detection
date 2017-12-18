20def7168d6dd97b2b310df5d6dbfa5b9aad026a
taopipi <taoiwasaki.dev@gmail.com>
2017-12-19 06:04:39 +0900

    実験コード実装途中

--- expt_modules.py
+++ expt_modules.py
@@ -1,4 +1,7 @@
 import enum
+import os
+import my_file_system as myfsys
+from my_file_system import DirNames
 
 class Features(enum.Enum):
     SIFT = enum.auto()
@@ -11,3 +14,13 @@ class Features(enum.Enum):
     AKAZE_FLANN = enum.auto()
     ORB_FLANN = enum.auto()
     BRISK_FLANN = enum.auto()
+
+
+def setup_expt_directory(base_name):
+    outputs_dir = myfsys.get_dir_path_(DirNames.OUTPUTS.value)
+    expt_name, ext = os.path.splitext(base_name)
+    expt_path = os.path.join(outputs_dir, expt_name)
+    if os.path.exists(expt_path):
+        return expt_path
+    os.mkdir(expt_path)
+    return expt_path
