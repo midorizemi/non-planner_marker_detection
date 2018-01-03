ce28a49218ae3c1803c8911dbf5ee9e1c5261b1e
taopipi <taoiwasaki.dev@gmail.com>
2018-01-03 21:57:43 +0900

    add cmmon modules

--- commons/__init__.py
+++ commons/__init__.py
@@ -3,4 +3,6 @@ from . import find_obj
 from . import template_info
 from . import affine_base
 from . import custom_find_obj
+from . import my_file_path_manager
+from . import expt_modules
 from . import test
