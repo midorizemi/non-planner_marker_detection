6d39915889aed04a55462089a9d37d1737d32759
taopipi <taoiwasaki.dev@gmail.com>
2018-01-14 19:41:20 +0900

    途中

--- make_database/__init__.py
+++ make_database/__init__.py
@@ -1,4 +1,5 @@
 from . import asift
-from . import split_affinesim as spltA
+from . import split_affinesim
 from . import split_affinesim2nd as spltA2
+from . import make_splitmap
 from . import test
---
+++ make_database/split_affinesim_combinable.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+
+'''
+USAGE
+  split_affinesim.py [--feature=<sift|surf|orb|brisk>[-flann]] [ <image1> <image2> ]
+
+  --feature  - Feature to use. Can be sift, surf, orb or brisk. Append '-flann'
+               to feature name to use Flann-based matcher instead bruteforce.
+
+  Press left mouse button on a feature point to see its matching point.
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+# built-in modules
+from multiprocessing.pool import ThreadPool
+
+import cv2
+import numpy as np
+
+# local modules
+from commons.common import Timer
+from commons.find_obj import init_feature
+from commons.affine_base import affine_detect
+from commons.template_info import TemplateInfo as TmpInf
+from commons.custom_find_obj import explore_match_for_meshes, filter_matches_wcross as c_filter
+from commons.custom_find_obj import calclate_Homography, calclate_Homography_hard, draw_matches_for_meshes
+from make_database import make_splitmap as mks
+import make_database.split_affinesim as splaf
+
+def split_kd(keypoints, descrs, splt_num):
+    tmp = TmpInf()
+    split_tmp_img = mks.make_splitmap(tmp)
+    assert isinstance(split_tmp_img, np.ndarray)
+    global descrs_list
+    if isinstance(descrs, np.ndarray):
+        descrs_list = descrs.tolist()
+    splits_k = [[] for row in range(splt_num)]
+    splits_d = [[] for row in range(splt_num)]
+
+    assert isinstance(keypoints, list)
+    assert isinstance(descrs_list, list)
+    for keypoint, descr in zip(keypoints, descrs_list):
+        x, y = np.int32(keypoint.pt)
+        if x < 0 or x >= 800:
+            if x < 0:
+                x = 0
+            else:
+                x = 799
+        if y < 0 or y >= 600:
+            if y < 0:
+                y = 0
+            else:
+                y = 599
+        splits_k[split_tmp_img[y, x][0]].append(keypoint)
+        splits_d[split_tmp_img[y, x][0]].append(descr)
+
+    for i, split_d in enumerate(splits_d):
+        splits_d[i] = np.array(split_d, dtype=np.float32)
+
+    return splits_k, splits_d
+
+def combine_mesh(split_k, split_d, scols, srows):
+    mesh_ids = np.arange(scols * srows)
+    np.reshape(mesh_ids, (srows, scols))
+
+    def check_amounts(k):
+
+def affine_detect_into_mesh(detector, split_num, img1, mask=None, simu_param='default'):
+    pool = ThreadPool(processes=cv2.getNumberOfCPUs())
+    kp, desc = affine_detect(detector, img1, mask, pool=pool, simu_param=simu_param)
+    return split_kd(kp, desc, split_num)
+
+def match_with_cross(matcher, meshList_descQ, meshList_kpQ, descT, kpT):
+    meshList_pQ = []
+    meshList_pT = []
+    meshList_pairs = []
+    for mesh_kpQ, mesh_descQ in zip(meshList_kpQ, meshList_descQ):
+        raw_matchesQT = matcher.knnMatch(mesh_descQ, trainDescriptors=descT, k=2)
+        raw_matchesTQ = matcher.knnMatch(descT, trainDescriptors=mesh_descQ, k=2)
+        pQ, pT, pairs = c_filter(mesh_kpQ, kpT, raw_matchesQT, raw_matchesTQ)
+        meshList_pT.append(pT)
+        meshList_pQ.append(pQ)
+        meshList_pairs.append(pairs)
+    return meshList_pQ, meshList_pT, meshList_pairs
+
+def count_keypoints(splt_kpQ):
+    len_s_kp = 0
+    for kps in splt_kpQ:
+        len_s_kp += len(kps)
+    return len_s_kp
+
+if __name__ == '__main__':
+    print(__doc__)
+
+    import sys, getopt
+    opts, args = getopt.getopt(sys.argv[1:], '', ['feature='])
+    opts = dict(opts)
+    feature_name = opts.get('--feature', 'sift')
+    try:
+        fn1, fn2 = args
+    except:
+        import os
+        dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+        fn1 = os.path.abspath(os.path.join(dir, 'data/templates/qrmarker.png'))
+        fn2 = os.path.abspath(os.path.join(dir, 'data/inputs/unittest/smpl_1.414214_152.735065.png'))
+
+    imgQ = cv2.imread(fn1, 0)
+    imgT = cv2.imread(fn2, 0)
+    detector, matcher = init_feature(feature_name)
+
+    if imgQ is None:
+        print('Failed to load fn1:', fn1)
+        sys.exit(1)
+
+    if imgT is None:
+        print('Failed to load fn2:', fn2)
+        sys.exit(1)
+
+    if detector is None:
+        print('unknown feature:', feature_name)
+        sys.exit(1)
+
+    splt_num = 64
+    print('using', feature_name)
+    splt_kpQ, splt_descQ = affine_detect_into_mesh(detector, splt_num, imgQ, simu_param='default')
+    pool = ThreadPool(processes=cv2.getNumberOfCPUs())
+    kpT, descT = affine_detect(detector, imgT, pool=pool, simu_param='test')
+    print('imgQ - %d features, imgT - %d features' % (count_keypoints(splt_kpQ), len(kpT)))
+
+    with Timer('matching'):
+        mesh_pQ, mesh_pT, mesh_pairs = match_with_cross(matcher, splt_descQ, splt_kpQ, descT, kpT)
+
+    Hs = []
+    statuses = []
+    kp_pairs_long = []
+    Hs_stable = []
+    kp_pairs_long_stable = []
+    for pQ, pT, pairs in zip(mesh_pQ, mesh_pT, mesh_pairs):
+        pairs, H, status = calclate_Homography(pQ, pT, pairs)
+        Hs.append(H)
+        statuses.append(status)
+        if np.sum(status)/len(status) >= 0.4:
+            Hs_stable.append(H)
+        else:
+            Hs_stable.append(None)
+        for p in pairs:
+            kp_pairs_long.append(p)
+            if np.sum(status)/len(status) >= 0.4:
+                kp_pairs_long_stable.append(p)
+
+    vis = draw_matches_for_meshes(imgQ, imgT, Hs=Hs)
+    cv2.imshow('view weak meshes', vis)
+    cv2.imwrite('qr1_meshes.png', vis)
+
+    visS = draw_matches_for_meshes(imgQ, imgT, Hs=Hs_stable)
+    cv2.imshow('view stable meshes', visS)
+    cv2.imwrite('qr1_meshes_stable.png', visS)
+
+    viw = explore_match_for_meshes('affine find_obj', imgQ, imgT, kp_pairs_long_stable, Hs=Hs_stable)
+    cv2.imwrite('qr1_mesh_line.png', viw)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
